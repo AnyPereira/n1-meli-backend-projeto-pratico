d7e12b866c7ee3b602beaed0e19b9034595bb109
Any <55221912+AnyPereira@users.noreply.github.com>
2019-10-22 21:55:19 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get("/", function(req, res){
     res.status(200).send({
-        title: "Projeto Pratico Reprograma Turma Meli",
+        title: "Reprograma Turma Meli",
         version: '0.0.1'
     })
 })
--- tarefasRoute.js
+++ tarefasRoute.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const controller = require("../controllers/tarefasController")
 
 router.get("/", controller.get)
+router.get("/colaborador",controller.getColaborador)
+router.get("/:id", controller.getById)
+router.get("/:id/nome",controller.getNome)
+
 
 module.exports = router
