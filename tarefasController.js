dd53f1df92f3e3c59d16a9496f5a6535e04579dd
Any <55221912+AnyPereira@users.noreply.github.com>
2019-10-22 21:55:42 -0300

    Add files via upload

--- tarefasController.js
+++ tarefasController.js
@@ -4,3 +4,30 @@ exports.get = (req,res) =>{
     res.status(200).send(tarefas)
 }
 
+exports.getById = (req,res) =>{
+    const id = req.params.id
+
+    tarefasId = tarefas.find(tarefas => tarefas.id == id)
+    if(!tarefasId){
+        res.send("Tarefa não encontrada!")
+    }
+    res.send(tarefasId)
+}
+
+exports.getColaborador = (req, res) => {
+    
+    const nomeConcluiu = tarefas.filter(tarefa => tarefa.concluido == "true")    
+    res.status(200).send(nomeConcluiu)
+}
+
+exports.getNome = (req,res) => {
+    const id = req.params.id
+    const colaboradorUnico = tarefas.find(tarefa => tarefa.id == id) 
+    const fez = tarefas.map( tarefa => tarefa.nomeColaborador)
+    res.status(200).send(fez)   
+}
+
+
+
+
+
